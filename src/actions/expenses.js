2a29080c629620334009211a4760cbc0d51503ad
luchos <luch05_77@hotmail.com>
2022-02-26 17:34:15 -0300

    store user data in user area

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -1,3 +1,4 @@
+import { auth } from 'firebase';
 import uuid from 'uuid';
 import database from '../firebase/firebase'
 
@@ -7,7 +8,8 @@ export const addExpense = (expense) => ({
 });
 
 export const startAddExpense = (expenseData = {}) => {
-    return (dispatch) => {
+    return (dispatch, getState) => {
+        const uid = getState().auth.uid;
         const {
             description = '',
             note = '',
@@ -17,7 +19,7 @@ export const startAddExpense = (expenseData = {}) => {
 
         const expense = { description, note, amount, createAt};
 
-        return database.ref('expenses').push(expense).then((ref) => {
+        return database.ref(`users/${uid}/expenses`).push(expense).then((ref) => {
             dispatch(addExpense({
                 id: ref.key,
                 ...expense
@@ -31,11 +33,12 @@ export const removeExpense = ({id} = {}) => ({
     id
 });
 
-export const startRemoveExpense = ({ id } = {}) => {
-    return (dispatch) => {
-      return database.ref(`expenses/${id}`).remove().then(() => {
-        dispatch(removeExpense({ id }));
-      });
+export const startRemoveExpense = ({ id } = {}) => {    
+    return (dispatch, getState) => {
+        const uid = getState().auth.uid;
+        return database.ref(`users/${uid}/expenses/${id}`).remove().then(() => {
+            dispatch(removeExpense({ id }));
+        });
     };
 };
 
@@ -45,9 +48,10 @@ export const editExpense = (id, updates) => ({
     updates
 });
 
-export const startEditExpense = (id, updates) => {
-    return (dispatch) => {
-        return database.ref(`expenses/${id}`).update(updates).then(() => {
+export const startEditExpense = (id, updates) => {    
+    return (dispatch, getState) => {
+        const uid = getState().auth.uid;
+        return database.ref(`users/${uid}/expenses/${id}`).update(updates).then(() => {
             dispatch(editExpense(id, updates));
         });
     };
@@ -59,8 +63,9 @@ export const setExpenses = (expenses) => ({
 });
 
 export const startSetExpenses = () => {
-    return (dispatch) => {
-        return database.ref('expenses').once('value').then((snapshot) => {
+    return (dispatch, getState) => {
+        const uid = getState().auth.uid;
+        return database.ref(`users/${uid}/expenses`).once('value').then((snapshot) => {
             const expenses = [];
 
             snapshot.forEach((childSnapshot) => {
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,9 +6,9 @@ import { startLogout } from "../actions/auth";
 export const Header = ({ startLogout }) => (
     <header>
         <h1>Expensify</h1>
-        <NavLink to="/" activeClassName='is-active' exact={true}>home   </NavLink>
+        <NavLink to="/dashboard" activeClassName='is-active' >home   </NavLink>
         <NavLink to="/create" activeClassName='is-active'>create  </NavLink>
-        <NavLink to="/help" activeClassName='is-active'>help</NavLink>    
+        {/* <NavLink to="/help" activeClassName='is-active'>help</NavLink>     */}
         <button onClick={startLogout}>Logout</button>    
     </header>
 );
